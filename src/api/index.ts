3e132dd357a4434e71b28ddb320715868d475b1f
hobang-kor <67723384+hobang-kor@users.noreply.github.com>
2023-06-05 16:45:23 +0900

    perfect

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,31 +1,36 @@
-import axios from 'axios';
+import axios, { AxiosResponse } from 'axios';
 import { IPostWithAd, IResponsePostList } from './types';
 
 const instance = axios.create({
   headers: {
     'Content-Type': 'application/json',
   },
-  baseURL: '',
+  baseURL: 'http://localhost:8000/',
+  //http://34.22.77.64:8080
 });
 
 // todo (6) api 작성
 
-export const getPostList = () => {
-  return null;
+export const getPostList = (): Promise<AxiosResponse<IResponsePostList>> => {
+  return instance.get('/posts');
 };
 
-export const createPost = () => {
-  return null;
+export const createPost = (title: string, contents: string, tag: string) => {
+  return instance.post('/posts', {
+    title,
+    contents,
+    tag,
+  });
 };
 
-export const getPostById = () => {
-  return null;
+export const getPostById = (id: number) => {
+  return instance.get(`/post/${id}`);
 };
 
-export const updatePostById = () => {
-  return null;
+export const updatePostById = (id: number) => {
+  return instance.post(`/post/${id}`); //이건 내가 만듬
 };
 
-export const deletePostById = () => {
-  return null;
+export const deletePostById = (id: number) => {
+  return instance.delete(`/post/${id}`); //이건 내가 만듬
 };
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -32,7 +32,27 @@ const list = [
 ];
 
 const Home = () => {
-  return <div>{/*todo (3-1) post 목록 작성*/}</div>;
+  const [postList, setPostList] = useState<IResponsePostList>([]);
+
+  const fetchPostList = async () => {
+    const { data } = await getPostList();
+    setPostList(data);
+  };
+
+  useEffect(() => {
+    fetchPostList();
+  }, []);
+
+  if (postList.length === 0) {
+    return <NoPostList />;
+  }
+  return (
+    <div>
+      {postList.map(({ post }, index) => (
+        <PostListItem key={index} id={post.id} title={post.title} contents={post.contents} tag={post.tag} />
+      ))}
+    </div>
+  );
 };
 
 export default Home;
